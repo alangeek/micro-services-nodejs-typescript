6ffcca8a557125449292debfbb438e707b3eca92
Alan Christian <alanchristiancreator@hotmail.com>
2021-10-12 19:11:03 -0300

    feat: add typescript on develop

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Olá lindo')
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+const port = 3000
+
+app.use('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'Fodaa' })
+})
+
+app.listen(3000, () => {
+  console.log(` Start serve on ${port}`)
+})
